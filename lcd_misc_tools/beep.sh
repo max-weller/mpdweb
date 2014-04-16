821914e4424ba227ed566ca14947d4e6a5bf5086
Max Weller <git@max-weller.de>
2014-04-16 12:19:16 +0200

    photos

--- lcd_misc_tools/beep.sh
+++
@@ -1,1 +0,0 @@
-sudo ./beep 1000 ; sudo ./beep 800 ; sudo ./beep 600
--- lcd_misc_tools/config-template.js
+++
@@ -1,8 +0,0 @@
-exports.imapOptions = {
-    user: '<<< insert imap username here >>>',
-    password: '<<< insert imap password here >>>',
-    host: '<<< insert host here >>>',
-    port: 143,
-    tls: false,
-    tlsOptions: { rejectUnauthorized: false },
-};
